c80384c30e711e3041098868876ce4e8e9d2a456
Tri Nguyen <tri@tridnguyen.com>
2013-06-10 16:14:13 -0400

    render some transactions, add transaction form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,13 +9,23 @@ angular.module('uniApp')
 		];
 		$scope.transactions = [
 		// storing currency as integer
-		{date: new Date("Mon Jun 10 2013"), merchant: "Stop & Shop", item_desc: "Groceries", amount: 10123, type: "Groceries", source: "Reading Personal"},
-		{date: new Date("Sun Jun 9 2013"), merchant: "Dunkin Donut", item_desc: "Coffee", amount: 354, type: "Dine Out", source: "Reading Business"}
-	];
+			{date: new Date("Mon Jun 10 2013"), merchant: "Stop & Shop", item_desc: "Groceries", amount: 10123, type: "Groceries", source: "Reading Personal"},
+			{date: new Date("Sun Jun 9 2013"), merchant: "Dunkin Donut", item_desc: "Coffee", amount: 354, type: "Dine Out", source: "Reading Business"}
+		];
+		$scope.types = [
+			{name: "Choose a type", value: ""},
+			{name: "Credit Card", value: "credit-card"},
+			{name: "Groceries", value: "groceries"},
+			{name: "Gas", value: "gas"},
+			{name: "Tax", value: "tax"},
+			{name: "Insurance", value: "insurance"}
+		];
+		$scope.sources = [
+			{name: "Choose a source", value: ""},
+			{name: "Reading Business", value: "reading-business"},
+			{name: "Reading Personal", value: "reading-personal"}
+		];
+		$scope.addTransaction = function() {
+			// $scope.transactions.
+		}
 	});
-
-function MainCtrl($scope) {
-
-
-
-}
